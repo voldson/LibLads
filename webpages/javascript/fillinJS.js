3e60de063059770e4abb014bb049131e3ebf87e5
Victoria Oldson <voldson@purdue.edu>
2018-04-24 15:52:47 -0400

    fillin array of words, doesn't work yet

--- webpages/javascript/fillinJS.js
+++ webpages/javascript/fillinJS.js
@@ -1,11 +1,17 @@
 // Get page elements
 const seeStoryButton = document.getElementById("seeStoryButton");
+var boxesTab = document.getElementById("words");
 
 // Add event listeners
-seeStoryButton.addEventListener("click", goToSeeStoryScreen);
+//seeStoryButton.addEventListener("click", goToSeeStoryScreen);
+seeStoryButton.addEventListener("click", createArray)
 
 // Callbacks
 function goToSeeStoryScreen() {
     // window.location.href = "../html/seestory.html";
     window.location.href = "story.html";
+}
+
+function createArray() {
+	alert(boxesTab.rows[0].cells[0].value);
 }
