29ca65d4e5370a4c1e60d4d7e69acf29cd145192
Victoria Oldson <voldson@purdue.edu>
2018-04-23 22:54:16 -0400

    story html fixes

---
+++ webpages/javascript/storyFilledInJS.js
@@ -0,0 +1,8 @@
+//story placeholder
+var ph = 'This is your story';
+
+var div = document.getElementById("mainDiv");
+var str = '<label>Your Story!</label><p>';
+str += ph;
+str += '</p>';
+div.innerHTML = str;
